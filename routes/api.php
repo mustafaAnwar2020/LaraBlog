f8829367ab46f58deeb36452b85a646acf4f3c43
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-01-13 05:51:17 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('register','App\Http\Controllers\API\registerController@register');
 Route::post('login','App\Http\Controllers\API\loginController@login');
+
+
+Route::middleware('auth:api')->group(function(){
+    Route::resource('Posts','App\Http\Controllers\API\postsController');
+    Route::resource('Profile','App\Http\Controllers\API\profileController');
+    Route::get('Post/restore/{id}','App\Http\Controllers\API\postsController@restoreTrashed');
+    Route::get('Post/delete/{id}','App\Http\Controllers\API\postsController@delete');
+});
