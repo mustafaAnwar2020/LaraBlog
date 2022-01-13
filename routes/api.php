8518a3294fe12e86d964cd2ae3e6846f9b759306
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-01-13 10:18:03 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,10 @@ Route::post('login','App\Http\Controllers\API\loginController@login');
 
 Route::middleware('auth:api')->group(function(){
     Route::resource('Posts','App\Http\Controllers\API\postsController');
+    Route::resource('Comments','App\Http\Controllers\API\commentController');
+    Route::get('Comment/{id}','App\Http\Controllers\API\commentController@index');
+    Route::get('Reply/{id}','App\Http\Controllers\API\commentController@replyIndex');
+    Route::post('reply','App\Http\Controllers\API\commentController@replyStore');
     Route::resource('Profile','App\Http\Controllers\API\profileController');
     Route::get('Post/restore/{id}','App\Http\Controllers\API\postsController@restoreTrashed');
     Route::get('Post/delete/{id}','App\Http\Controllers\API\postsController@delete');
